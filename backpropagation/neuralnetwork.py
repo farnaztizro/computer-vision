45c6725e12a77d8daf35babf4ff24ad3b26fb4d8
farnaz tizro <far.tizro@gmail.com>
2021-01-30 14:29:10 +0330

    Implementing Backpropagation

--- backpropagation/neuralnetwork.py
+++ backpropagation/neuralnetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork:
     # alpha value is applied during the weight update phase
-    def __init__(self, layers, alpha):
+    def __init__(self, layers, alpha=0.1):
 
         # initializes our list of weights for each layer
         self.W = []
@@ -15,9 +15,9 @@ class NeuralNetwork:
             w = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
             self.W.append(w / np.sqrt(layers[i]))
 
-            # where the input connections need a bias term, but the output does not
-            w = np.random.randn(layers[-2] + 1, layers[-1])
-            self.W.append(w / np.sqrt(layers[-2]))
+        # where the input connections need a bias term, but the output does not
+        w = np.random.randn(layers[-2] + 1, layers[-1])
+        self.W.append(w / np.sqrt(layers[-2]))
 
     # useful for debugging
     def __repr__(self):
